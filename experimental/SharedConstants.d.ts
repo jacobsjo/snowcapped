d947f8b4cda882b105f39ff3947920e8ee78960b
Jochen Jacobs <jochen.jacobs@campus.tu-berlin.de>
2022-05-28 03:38:18 +0200

    Updates

--- experimental/SharedConstants.d.ts
+++ experimental/SharedConstants.d.ts
@@ -1,4 +1,4 @@
-export declare const SNOWCAPPED_VERSION_ID = 4;
+export declare const SNOWCAPPED_VERSION_ID = 5;
 export declare const DATA_VERSION = 4;
 export declare const IS_EXPERIMENTAL = true;
 //# sourceMappingURL=SharedConstants.d.ts.map
