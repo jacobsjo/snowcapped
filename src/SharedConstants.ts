44cbc4446b99c5f19a39e7109a118aa5aa230788
Jochen Jacobs <jochen.jacobs@campus.tu-berlin.de>
2022-11-16 00:03:35 +0100

    fix caching
    fixed #15

--- src/SharedConstants.ts
+++ src/SharedConstants.ts
@@ -1,7 +1,7 @@
 
-export const SNOWCAPPED_VERSION_ID = 7
+export const SNOWCAPPED_VERSION_ID = 8
 
 export const DATA_VERSION = 5
 export const IS_EXPERIMENTAL = false
 
-export const IS_DEV = true
+export const IS_DEV = false
