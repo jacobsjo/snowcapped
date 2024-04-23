60c11614e3c21abe9c237020d8aa5bf8145e8bcc
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-04-23 22:57:28 +0200

    update to minecraft 1.20.5

--- src/SharedConstants.ts
+++ src/SharedConstants.ts
@@ -1,4 +1,4 @@
 export const DATA_VERSION = 6
-export const DEFAULT_DATAPACK_FORMAT = 26
+export const DEFAULT_DATAPACK_FORMAT = 41
 export const MIN_DATAPACK_FORMAT = 10
-export const MAX_DATAPACK_FORMAT = 26
+export const MAX_DATAPACK_FORMAT = 41
